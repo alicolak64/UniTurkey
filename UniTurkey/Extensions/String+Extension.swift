8daac7c01446039b2958f632f32339a704e41743
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-04-07 08:52:09 +0300

    Added new extensions

---
+++ UniTurkey/Extensions/String+Extension.swift
@@ -0,0 +1,48 @@
+//
+//  String+Extension.swift
+//  UniTurkey
+//
+//  Created by Ali Çolak on 7.04.2024.
+//
+
+import Foundation
+
+extension String {
+    
+    var capitalizedEachWord: String {
+        let words = self.components(separatedBy: " ")
+        var capitalizedWords = [String]()
+        for word in words {
+            if let firstLetter = word.first {
+                let capitalizedWord = String(firstLetter).uppercased() + word.dropFirst()
+                capitalizedWords.append(capitalizedWord)
+            }
+        }
+        return capitalizedWords.joined(separator: " ")
+    }
+    
+    @discardableResult
+    mutating func capitalizeEachWord() -> String {
+        self = self.components(separatedBy: " ")
+            .map { $0.prefix(1).uppercased() + $0.dropFirst() }
+            .joined(separator: " ")
+        return self
+    }
+    
+    var isValidUrl: Bool {
+        return URL(string: self) != nil
+    }
+    
+    var url: URL? {
+        return URL(string: self)
+    }
+    
+    var int: Int? {
+        return Int(self)
+    }
+    
+    var trimmed: String {
+        return trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+}
