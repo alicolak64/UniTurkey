e4da2acb90f77c611e59d1a1df501457245c654c
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-04-08 03:12:48 +0300

    Added App Constans

---
+++ UniTurkey/Resources/Constants/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  UniTurkey
+//
+//  Created by Ali Çolak on 8.04.2024.
+//
+
+import UIKit
+
+// MARK: - App Constants
+enum AppConstants {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+    static let infinityScrollPercentage = 0.8
+    static let infinityScrollLateLimitSecond = 1.0
+}
