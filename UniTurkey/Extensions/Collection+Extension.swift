c9bd5b77dfaab2fb35b2de7ea3c3933d7e0ab6ae
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-04-10 04:40:47 +0300

    Changed name CollectionExtension

---
+++ UniTurkey/Extensions/Collection+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionExtension.swift
+//  UniTurkey
+//
+//  Created by Ali Çolak on 7.04.2024.
+//
+
+import Foundation
+
+extension Collection {
+    
+    // MARK: - Properties(Safe Access)
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
