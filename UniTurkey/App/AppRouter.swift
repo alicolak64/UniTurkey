dbac839ab6a3d7dfdf284afb07d9caf8860029fd
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-04-19 07:10:12 +0300

    Added splash screen delay

--- UniTurkey/App/AppRouter.swift
+++ UniTurkey/App/AppRouter.swift
@@ -10,20 +10,31 @@ import UIKit
 final class AppRouter {
     
     // MARK: - Properties
+    
     let window: UIWindow
     
     // MARK: - Initializers
+    
     init() {
         window = UIWindow(frame: UIScreen.main.bounds)
     }
     
     // MARK: - Start
+    
     func start() {
-        let homeViewController = HomeBuilder().build()
-        let navigationController = app.navigationController
-        navigationController.viewControllers = [homeViewController]
-        window.rootViewController = navigationController
+        
+        window.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
         window.makeKeyAndVisible()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in // Wait 3 seconds for launch screen
+            guard let self = self else { return }
+            let homeViewController = HomeBuilder().build()
+            let navigationController = app.navigationController
+            navigationController.viewControllers = [homeViewController]
+            window.rootViewController = navigationController
+        }
+        
     }
+    
 }
 
